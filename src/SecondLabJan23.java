4a02abafe2d5bfbed67c2991f89ce5d073fec1fd
Rzshep <35359229+Rzshep@users.noreply.github.com>
2018-01-23 22:16:17 -0500

    Update SecondLabJan23.java
    - I updated the conditional statements within the while loop so that they all print the option for the user to continue or not to continue.
    - The conditional statements within the while loop now also have values for userChar that are based on user input.
    - Comments were added.

--- src/SecondLabJan23.java
+++ src/SecondLabJan23.java
@@ -4,9 +4,9 @@ public class SecondLabJan23 {
 
 	public static void main(String[] args) {
 		
-		char userChar;
+		char userChar; // declare char userChar
 		
-		userChar = 'y';
+		userChar = 'y'; // make 'y' userChar
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -18,39 +18,48 @@ public class SecondLabJan23 {
 		
 		
 		
-		while (userChar == 'y' || userChar == 'Y') {
+		while (userChar == 'y' || userChar == 'Y') { // use while loop to begin
 			
 			System.out.println("Okay, " + name + "," + " enter an integer between 1 and 100.");
 			
 			int firstInt = scan.nextInt();
 		
-		if (firstInt >= 101 || firstInt <= 0) {
-			System.out.println(name + "," + " what did I say?!? I told you to enter an integer between 1 and 100!"
-					+ " Would you like to continue? (y/n): ");
-			userChar = scan.next().charAt(0);
+		if (firstInt >= 101 || firstInt <= 0) { // the conditional statements are a part of the while loop
+			System.out.println(name + "," + " what did I say?!? I told you to enter an integer between 1 and 100! Would you like to continue? (y/n): ");
+			userChar = scan.next().charAt(0); // user inputs userChar
 			
 			
-		}
-		else if (firstInt % 2 != 0 && firstInt <= 60) {
+		} else if (firstInt % 2 != 0 && firstInt <= 60) {
 			System.out.println(firstInt + " Odd");
-			break;
-	}	else if (firstInt % 2 == 0 && firstInt <= 25 && firstInt >= 2) {
+			System.out.println("Would you like to continue? (y/n): ");
+			userChar = scan.next().charAt(0);
+			
+		} else if (firstInt % 2 == 0 && firstInt <= 25 && firstInt >= 2) {
 			System.out.println("Even and less than 25.");
-			System.out.println("Would you like to continue?");
+			System.out.println("Would you like to continue? (y/n): ");
 			userChar = scan.next().charAt(0);
 			
 			
-	}	else if (firstInt % 2 == 0 && firstInt <= 60 && firstInt >= 26) {
+		} else if (firstInt % 2 == 0 && firstInt <= 60 && firstInt >= 26) {
 			System.out.println("Even");
-	}	else if (firstInt % 2 == 0 && firstInt >= 60) {
+			System.out.println("Would you like to continue? (y/n): ");
+			userChar = scan.next().charAt(0);
+			
+		} else if (firstInt % 2 == 0 && firstInt >= 60) {
 			System.out.println(firstInt + " Even");
-	}	else if (firstInt >= 61) {
-			System.out.println(firstInt + "Odd and over 60.");
-	}
+			System.out.println("Would you like to continue? (y/n): ");
+			userChar = scan.next().charAt(0);
+			
+		} else if (firstInt >= 61) {
+			System.out.println(firstInt + " Odd and over 60.");
+			System.out.println("Would you like to continue? (y/n): ");
+			userChar = scan.next().charAt(0);
+		}
 
-}	 if (userChar == 'n' || userChar == 'N') {
-		System.out.println("Goodbye!");}
+		} if (userChar == 'n' || userChar == 'N') { // this conditional statement is executed outside of the while loop and can only be executed once the while loop no longer runs
+			System.out.println("Goodbye!");}		
 		
-	}
-}
+		}
+		}
+
 
